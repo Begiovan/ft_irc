68c122a413e7b6390af637fdad9e0cd6aa8933ac
Susanna Kayed <kefka.sk@gmail.com>
2026-08-10 12:31:08 +0200

    Add: numeric reply topic related

--- irc_response.hpp
+++ irc_response.hpp
@@ -33,7 +33,8 @@
 #define RPL_WELCOME(source)                             "001 " + source + " :Welcome " + source + " to the ft_irc network"
 #define RPL_NAMREPLY(source, channel, users)            "353 " + source + " = " + channel + " :" + users
 #define RPL_ENDOFNAMES(source, channel)                 "366 " + source + " " + channel + " :End of /NAMES list."
-
+#define RPL_NOTOPIC(source, channel)					"331" + source + " " + channel + " :No topic set."
+#define RPL_TOPIC(source, channel, topic)				"332" + source + " " + channel + " :" + topic
 
 /* Command Responses */
 
